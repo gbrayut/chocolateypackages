a5cb6d7d96f9d64f17ef0b7c07690781b3aa28b2
Greg Bray <Greg.Bray@Gmail.com>
2013-10-25 15:32:17 -0600

    Updated to 4.2.0 release

--- diffmerge/tools/chocolateyInstall.ps1
+++ diffmerge/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$name   = 'diffmerge'
-$url    = 'http://download-us.sourcegear.com/DiffMerge/4.1.0/DiffMerge_4.1.0.534.stable_x86.msi'
-$url64  = 'http://download-us.sourcegear.com/DiffMerge/4.1.0/DiffMerge_4.1.0.534.stable_x64.msi'
+$url    = 'http://download-us.sourcegear.com/DiffMerge/4.2.0/DiffMerge_4.2.0.697.stable_x86.msi'
+$url64  = 'http://download-us.sourcegear.com/DiffMerge/4.2.0/DiffMerge_4.2.0.697.stable_x64.msi'
 $kind   = 'MSI'
 $silent = '/quiet'
 
